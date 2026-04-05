0b9eb1dc9c9c6c703be57710410f1e68fb9b80e4
sarahllg <a.sarah315@gmail.com>
2026-04-05 23:18:14 +0200

    redirige en fonction du role

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -15,18 +16,39 @@
             <div class="nav-links">
                 <a href="index.php">Accueil</a>
                 <a href="menu.php">Menu</a>
-                <a href="notation.php">Notation</a>
-                <a href="connexion.php">Se connecter</a>
-                <a href="inscription.php">S'inscrire</a>
 
+                <?php if (isset($_SESSION['auth'])) { ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'admin') { ?>
+                <a href="admin.php">Administration</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'restaurateur') { ?>
+                <a href="restaurateur.php">Commandes</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'livreur') { ?>
+                <a href="livreur.php">Ma livraison</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'client') { ?>
+                <a href="notation.php">Notation</a>
                 <div class="dropdown">
-                    <a href="" class="nav-links">Profil ⏷</a>
+                    <a href="#" class="nav-links">Profil ⏷</a>
                     <div class="dropdown-content">
                         <a href="informations.php">Mes informations</a>
                         <a href="commandes.php">Mes commandes</a>
                         <a href="compte+.php">Mon compte Bella Ciao +</a>
                     </div>
                 </div>
+                <?php } ?>
+
+                <a href="deconnexion.php">Se déconnecter</a>
+
+                <?php } else { ?>
+                <a href="connexion.php">Se connecter</a>
+                <a href="inscription.php">S'inscrire</a>
+                <?php } ?>
 
             </div>
         </div>
