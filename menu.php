7eefb6cbd637a229213521457f53f3fb7ed8de05
sarahllg <a.sarah315@gmail.com>
2026-04-05 11:34:11 +0200

    ajout deconnecté et admin

--- menu.php
+++ menu.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $json = file_get_contents("plats.json");
 $data = json_decode($json, true);
 $plats = $data['plats'];
@@ -22,17 +23,39 @@ $plats = $data['plats'];
             <div class="nav-links">
                 <a href="index.php">Accueil</a>
                 <a href="menu.php">Menu</a>
+
+                <?php if (isset($_SESSION['auth'])) { ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'admin') { ?>
+                <a href="admin.php">Administration</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'restaurateur') { ?>
+                <a href="restaurateur.php">Commandes</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'livreur') { ?>
+                <a href="livreur.php">Ma livraison</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'client') { ?>
                 <a href="notation.php">Notation</a>
-                <a href="connexion.php">Se connecter</a>
-                <a href="inscription.php">S'inscrire</a>
                 <div class="dropdown">
-                    <a href="" class="nav-links">Profil ⏷</a>
+                    <a href="#" class="nav-links">Profil ⏷</a>
                     <div class="dropdown-content">
                         <a href="informations.php">Mes informations</a>
                         <a href="commandes.php">Mes commandes</a>
                         <a href="compte+.php">Mon compte Bella Ciao +</a>
                     </div>
                 </div>
+                <?php } ?>
+
+                <a href="deconnexion.php">Se déconnecter</a>
+
+                <?php } else { ?>
+                <a href="connexion.php">Se connecter</a>
+                <a href="inscription.php">S'inscrire</a>
+                <?php } ?>
 
             </div>
         </div>
@@ -105,4 +128,5 @@ $plats = $data['plats'];
 </footer>
 
 </body>
+
 </html>
