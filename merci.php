68a263c14d1c64c07c180b8c751e9c45f4ef2951
sarahllg <a.sarah315@gmail.com>
2026-04-05 11:36:14 +0200

    ajout deconnecté et admin

--- merci.php
+++ merci.php
@@ -1,41 +1,64 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Merci - Bella Ciao</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body class="page-inscription">
-        <nav class="navbar">
-    <div class="container nav-content">
-        <div class="logo">Bella Ciao</div>
-        <div class="nav-links">
-            <a href="index.html">Accueil</a>
-            <a href="menu.html">Menu</a>
-            <a href="notation.html">Notation</a>
-            <a href="connexion.html">Se connecter</a>
-            <a href="inscription.html">S'inscrire</a>
-           
-            <div class="dropdown">
-                <a href="" class="nav-links">Profil ⏷</a>
-                <div class="dropdown-content">
-                    <a href="info.html">Mes informations</a>
-                    <a href="commandes.html">Mes commandes</a>
-                    <a href="compte+.html">Mon compte Bella Ciao +</a>
+    <nav class="navbar">
+        <div class="container nav-content">
+            <div class="logo">Bella Ciao</div>
+            <div class="nav-links">
+                <a href="index.php">Accueil</a>
+                <a href="menu.php">Menu</a>
+
+                <?php if (isset($_SESSION['auth'])) { ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'admin') { ?>
+                <a href="admin.php">Administration</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'restaurateur') { ?>
+                <a href="restaurateur.php">Commandes</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'livreur') { ?>
+                <a href="livreur.php">Ma livraison</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'client') { ?>
+                <a href="notation.php">Notation</a>
+                <div class="dropdown">
+                    <a href="#" class="nav-links">Profil ⏷</a>
+                    <div class="dropdown-content">
+                        <a href="informations.php">Mes informations</a>
+                        <a href="commandes.php">Mes commandes</a>
+                        <a href="compte+.php">Mon compte Bella Ciao +</a>
+                    </div>
                 </div>
-             </div>
+                <?php } ?>
 
+                <a href="deconnexion.php">Se déconnecter</a>
+
+                <?php } else { ?>
+                <a href="connexion.php">Se connecter</a>
+                <a href="inscription.php">S'inscrire</a>
+                <?php } ?>
+
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
-    
     <div class="merci-wrapper">
 
         <h1>Grazie mille ! 🇮🇹</h1>
 
         <p>
-            Merci d’avoir laissé un avis chez <b>Bella Ciao</b>.  
+            Merci d’avoir laissé un avis chez <b>Bella Ciao</b>.
             Votre opinion nous aide à nous améliorer chaque jour! ❤️
         </p>
 
@@ -43,10 +66,11 @@
 
     </div>
 
-    
+
     <footer>
         © 2026 Bella Ciao - Tous droits réservés
     </footer>
 
 </body>
+
 </html>
