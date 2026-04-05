5cb6acd787e5fce0dee41f67074404f26fcdac75
sarahllg <a.sarah315@gmail.com>
2026-04-05 11:37:50 +0200

    ajout deconnecté et admin

--- informations.php
+++ informations.php
@@ -1,33 +1,56 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
-    <head>
-        <title>Mes informations</title>
-        <link rel="stylesheet" href="style.css">
 
-     </head>
+<head>
+    <title>Mes informations</title>
+    <link rel="stylesheet" href="style.css">
 
-    <body class="page-inscription">
-        <nav class="navbar">
-    <div class="container nav-content">
-        <div class="logo">Bella Ciao</div>
-        <div class="nav-links">
-           <a href="index.php">Accueil</a>
-            <a href="menu.php">Menu</a>
-            <a href="notation.php">Notation</a>
-            <a href="connexion.php">Se connecter</a>
-            <a href="inscription.php">S'inscrire</a>
-           
-         <div class="dropdown">
-             <a href="" class="nav-links">Profil ⏷</a>
-         <div class="dropdown-content">
-    <a href="informations.php">Mes informations</a>
-    <a href="commandes.php">Mes commandes</a>
-    <a href="compte+.php">Mon compte Bella Ciao +</a>
-  </div>
-</div>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="container nav-content">
+            <div class="logo">Bella Ciao</div>
+            <div class="nav-links">
+                <a href="index.php">Accueil</a>
+                <a href="menu.php">Menu</a>
+
+                <?php if (isset($_SESSION['auth'])) { ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'admin') { ?>
+                <a href="admin.php">Administration</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'restaurateur') { ?>
+                <a href="restaurateur.php">Commandes</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'livreur') { ?>
+                <a href="livreur.php">Ma livraison</a>
+                <?php } ?>
+
+                <?php if ($_SESSION['auth']['role'] === 'client') { ?>
+                <a href="notation.php">Notation</a>
+                <div class="dropdown">
+                    <a href="#" class="nav-links">Profil ⏷</a>
+                    <div class="dropdown-content">
+                        <a href="informations.php">Mes informations</a>
+                        <a href="commandes.php">Mes commandes</a>
+                        <a href="compte+.php">Mon compte Bella Ciao +</a>
+                    </div>
+                </div>
+                <?php } ?>
+
+                <a href="deconnexion.php">Se déconnecter</a>
+
+                <?php } else { ?>
+                <a href="connexion.php">Se connecter</a>
+                <a href="inscription.php">S'inscrire</a>
+                <?php } ?>
+
+            </div>
         </div>
-    </div>
-</nav>
-
+    </nav>
     <div class="form-wrapper">
         <div class="nomrestaurant" id="entete-blanc">Mes Informations:</div>
         <br><br>
@@ -83,5 +106,5 @@
         <p>© 2026 Bella Ciao - Tous droits réservés</p>
     </footer>
 </body>
-</html>
 
+</html>
